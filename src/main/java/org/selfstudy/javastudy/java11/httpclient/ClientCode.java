af90b5ffc268fbfef690b68c48e502e799bf1ebd
kr2 <kamalendu@users.noreply.github.com>
2020-10-05 00:58:04 -0700

    Java 11 HttpClient code

---
+++ src/main/java/org/selfstudy/javastudy/java11/httpclient/ClientCode.java
@@ -0,0 +1,30 @@
+package org.selfstudy.javastudy.java11.httpclient;
+
+import lombok.extern.java.Log;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
+@Log
+public class ClientCode {
+
+    private static final HttpClient HTTP_CLIENT = HttpClient.newBuilder().connectTimeout(Duration.ofSeconds(10)).build();
+
+    public static void main(String[] args){
+        HttpResponse<String> httpResponse = null;
+        try{
+            String uri = "https://postman-echo.com/get";
+            HttpRequest httpRequest = HttpRequest.newBuilder(new URI(uri+"?foo1=bar1&foo2=bar2")).build();
+            httpResponse = HTTP_CLIENT.send(httpRequest,HttpResponse.BodyHandlers.ofString());
+        }catch (Exception ex){
+            log.severe("Exception occured: "+ex.getMessage());
+        }
+        log.info("Response Code: "+httpResponse.statusCode());
+        log.info("Response Header: "+httpResponse.headers());
+        log.info("Response Body: "+httpResponse.body());
+    }
+}
+
