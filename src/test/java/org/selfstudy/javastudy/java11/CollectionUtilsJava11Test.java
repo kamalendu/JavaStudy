9fed52d0ad90c8f673328e863523be4a114573ec
kr2 <kamalendu@users.noreply.github.com>
2020-10-12 22:09:39 -0700

    hibernate validation

--- src/test/java/org/selfstudy/javastudy/java11/CollectionUtilsJava11Test.java
+++ src/test/java/org/selfstudy/javastudy/java11/CollectionUtilsJava11Test.java
@@ -3,8 +3,11 @@ package org.selfstudy.javastudy.java11;
 import org.junit.Assert;
 import org.junit.Test;
 
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class CollectionUtilsJava11Test {
     @Test
@@ -17,4 +20,23 @@ public class CollectionUtilsJava11Test {
         Assert.assertArrayEquals(new String[]{"Bhola","Shiva","Sambhu"},strList.toArray(String[]::new));
 
     }
+
+    @Test
+    public void testPredicateNot(){
+        List<String> months = List.of("Jan","May","Aug","Dec");
+        List<String> mnList1 = months.stream().filter(str -> !str.startsWith("M")).collect(Collectors.toList());
+        List<String> mnList2 = months.stream().filter(((Predicate<String>) str -> str.startsWith("M")).negate()).collect(Collectors.toList());
+        List<String> mnList3 = months.stream().filter(Predicate.not(str -> str.startsWith("M"))).collect(Collectors.toList());
+
+        Assert.assertEquals(mnList1,mnList2);
+        Assert.assertEquals(mnList1,mnList3);
+    }
+
+    @Test
+    public void testVarInLambda(){
+        List<String> months = new ArrayList<>();
+        months.addAll(List.of("Jan","May","Aug","Dec"));
+        Assert.assertEquals(months.stream().filter( Predicate.not((var str) -> str.startsWith("M"))).collect(Collectors.toList()).size(),3);
+    }
+
 }
--- src/test/java/org/selfstudy/javastudy/model/CompanyTest.java
+++ src/test/java/org/selfstudy/javastudy/model/CompanyTest.java
@@ -3,13 +3,24 @@ package org.selfstudy.javastudy.model;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 
 public class CompanyTest {
 
-    private Company company = new Company("Indimade",new Address("Siva street","Dallas",State.TX,888088), 123L);
+    private final Company COMPANY = new Company("Indimade",new Address("Siva street","Dallas",State.TX,888088), 123L);
     @Test
-    public void getName() {
-        Assert.assertEquals("Indimade",company.getName());
+    public void testGetName() {
+        Assert.assertEquals("Indimade",COMPANY.getName());
+    }
+    @Test
+    public void testNameNotNullable() {
+        Company company1 = new Company(null,new Address("Siva street","Dallas",State.TX,888088), 123L);
+        // validate the input
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        var violations = validator.validate(company1);
+        Assert.assertTrue(violations.size()==1);
     }
 }
